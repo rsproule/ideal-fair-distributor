4fed2f49dc8af9a8d4fcadd2d40d46c8bca51c57
Ryan Sproule <rsproule23@gmail.com>
2022-01-21 16:21:06 -0800

    preference maximize commit reveal scheme

--- packages/hardhat-ts/contracts/PreferenceMaximizer.sol
+++ packages/hardhat-ts/contracts/PreferenceMaximizer.sol
@@ -8,14 +8,17 @@ import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZe
 contract PreferenceMaximizer is Ownable {
   uint totalCommitments = 0;
   uint totalReveals = 0;
-  uint expectedAgents;
+  // need to edit this before deploying with number of NFTs there are
+  uint constant expectedAgents = 4;
+  uint[expectedAgents][expectedAgents] preferenceMatrix;
+
   Phase currentPhase = Phase.Commit;
   enum Phase {
     Commit, 
     Reveal
   }
   mapping(address => bool) _whitelist;  
-  mapping(address => PreferenceCommitment) _commitments;
+  mapping(address => PreferenceCommitment) public _commitments;
   event Commit(address sender, bytes32 commitment);
 
   struct PreferenceCommitment {
@@ -28,7 +31,6 @@ contract PreferenceMaximizer is Ownable {
     for (uint i = 0; i < whitelist.length; i++) {
         _whitelist[whitelist[i]] = true;
     }
-    expectedAgents = whitelist.length;
   }
 
   /// @notice this needs to be done via a commit reveal scheme, otherwise people
@@ -39,10 +41,6 @@ contract PreferenceMaximizer is Ownable {
     totalCommitments += 1;
     _commitments[msg.sender] = PreferenceCommitment(msg.sender, preferenceCommitment, new uint[](expectedAgents));
     emit Commit(msg.sender, preferenceCommitment);
-    
-    if (totalCommitments == expectedAgents) {
-      currentPhase = Phase.Reveal;
-    }
   }
 
   function revealPreference(uint[] calldata preferences, bytes32 secret) public {
@@ -50,22 +48,61 @@ contract PreferenceMaximizer is Ownable {
 
     PreferenceCommitment storage commitment = _commitments[msg.sender];
 
-    // assert these are correct 
+    // assert the commitment matches this input
     require(keccak256(abi.encodePacked(msg.sender, preferences, secret)) == commitment.commitment, "Commitment does not match");
     // lock in this preference
     commitment.preferences = preferences;
-    
+
+    // might as well store this stuff in the format the optimizer prefers, tho theoretically if we 
+    // cared about gas this should totally happen in the offchain function
+    for (uint i = 0; i < preferences.length; i++) {
+      preferenceMatrix[totalReveals][i] = preferences[i];     
+    }
+
+    totalReveals += 1;
   }
 
   /// @notice this function is readonly, once all commit reveal is done. 
-  function optimalSolution() public {
+  function optimalSolution() public returns (uint[] memory){
     require(totalReveals == expectedAgents, "Missing reveals.");
+    return calculateOptimalMatching(); 
+  }
 
+
+  /// @notice this is a pure readonly function meant to be a helper for 
+  /// generating ur commitment
+  function makeCommitment(uint[] calldata preferences, bytes32 secret) public view returns (bytes32){
+    require(preferences.length == expectedAgents, "Incorrect length of preferences");
+    return keccak256(abi.encodePacked(msg.sender, preferences, secret)); 
 
   }
   
 
-  function calculateOptimalMatching() public {
-    // why do this in a smart contract, is there there any reason that 
+  function calculateOptimalMatching() public returns(uint[] memory) {
+    // // convert this to solidity
+    // uint solution = [];
+    // for (var i = 0; i < preferenceMatrix.length; i++) {
+    //   let minSum = Infinity;
+    //   let minSumIndex = -1;
+    //   for (var j = 0; j < preferenceMatrix[i].length; j++) {
+    //     if (solution.indexOf(j) !== -1) {
+    //       continue;
+    //     }
+    //     let sum = preferenceMatrix[i][j];
+    //     for (var ii = i; ii < preferenceMatrix.length; ii++) {
+    //       if (ii != i) {
+    //         let seenAndCurrentIndices = [j];
+    //         sum += getMinIgnoreIndices(preferenceMatrix[ii], seenAndCurrentIndices);
+    //       }
+    //     }
+    //     if (sum < minSum) {
+    //       minSum = sum;
+    //       minSumIndex = j;
+    //     }
+    //   }
+    //   solution.push(minSumIndex);
+    // }
+    // return solution;
+
   }
 }
--- packages/hardhat-ts/test/sample-test.ts
+++ packages/hardhat-ts/test/sample-test.ts
@@ -1,14 +1,42 @@
-const { expect } = require("chai");
+import { Contract, Signer } from "ethers";
+import { ethers } from "hardhat";
+import { SignerWithAddress } from "hardhat-deploy-ethers/signers";
+import { randomBytes } from "crypto";
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
+
+describe("Commit-Reveal-Preferences", async () => {
+  let prefMaximizerContract: Contract;
+
+  beforeEach(async () => {
+    // deploy the contract
+    const whitelist: SignerWithAddress[] = await ethers.getSigners();
+    const PreferenceMaximizer = await ethers.getContractFactory("PreferenceMaximizer")
+    prefMaximizerContract = await PreferenceMaximizer.deploy(whitelist.map(a => a.address));
     
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  })
+  it("Should commit a preference list", async () => {
+    
+    const preferences = [1, 2, 3, 4]
+    const secret = ""
+  })
 
-    await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+  it("should generate a commitment byte array from preference set and secret", async () => {
+    const preferences = [1, 2, 3, 4];
+    // const random = new Uint8Array(32);
+    const random = randomBytes(32)
+    const salt = "0x" + Array.from(random).map(b => b.toString(16).padStart(2, "0")).join("");
+
+    const commit = await prefMaximizerContract.makeCommitment(preferences, salt);
+    console.log(commit);
+    
+    //  submit the commitment
+    let res = await prefMaximizerContract.commitPreference(commit);
+
+    console.log(res);
+
+    let commitments = await prefMaximizerContract._commitments(res.from);
+    console.log(commitments.commit)
+    
+  })
+})
+
